1ddfa50a3f7b785a8f5dbaf3880054c0ca6207fb
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-04 22:15:16 +0530

    Contains Duplicate two ways

--- Arrays_and_Hashing.py
+++ Arrays_and_Hashing.py
@@ -1,11 +1,22 @@
+# def contains_duplicate_1(arr):  #TC-> O(n²)
 # Contains Duplicate
-def contains_duplicate(arr):
-    n=len(arr)
-    for i in range(n):
-        for j in range(i+1,n):
-            if arr[i]==arr[j]:
-                return True
+#     n=len(arr)
+#     for i in range(n):
+#         for j in range(i+1,n):
+#             if arr[i]==arr[j]:
+#                 return True
+#     return False
+
+# arr=[int(x) for x in input().split()]
+# print(contains_duplicate_1(arr))
+
+def contains_duplicate_2(arr):   #TC-> O(n)
+    array=set()
+    for i in arr:
+        if i in array:
+            return True
+        array.add(i)
     return False
 
 arr=[int(x) for x in input().split()]
-print(contains_duplicate(arr))
+print(contains_duplicate_2(arr))
