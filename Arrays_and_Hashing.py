88c79d3e25e5b81d9d9bf55e4231000644ef290d
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-06 14:45:42 +0530

    Valid Anagram three ways

--- Arrays_and_Hashing.py
+++ Arrays_and_Hashing.py
@@ -1,5 +1,5 @@
-# def contains_duplicate_1(arr):  #TC-> O(n²)
 # Contains Duplicate
+# def contains_duplicate_1(arr):  #TC-> O(n²)
 #     n=len(arr)
 #     for i in range(n):
 #         for j in range(i+1,n):
@@ -20,3 +20,50 @@
 
 # arr=[int(x) for x in input().split()]
 # print(contains_duplicate_2(arr))
+
+
+
+# Valid Anagram
+# Btter approach. TC-> O(logn) SC->O(n)
+# def valid_anagram_1(w1,w2):
+#     if(len(w1) != len(w2)):
+#         return False
+#     return "".join(sorted(w1))=="".join(sorted(w2))
+
+# w1=input()
+# w2=input()
+# print(valid_anagram_1(w1,w2))
+
+
+# # Optimal approach-Using Hashing. TC->:O(n) SC->: O(1)
+# from collections import Counter
+# def valid_anagram_2(w1,w2):
+#     if len(w1) != len(w2):
+#         return False
+
+#     if Counter(w1)==Counter(w2):
+#         return True
+#     return False
+
+# w1=input()
+# w2=input()
+# print(valid_anagram_2(w1,w2))
+
+#Another Approach
+#TC-> O(n²) SC->O(N)
+def valid_anagram_3(w1,w2):
+    if len(w1) != len(w2):
+        return False 
+    
+    word_list_2=list(w2)
+
+    for i in w1:
+        if i in word_list_2:
+            word_list_2.remove(i)
+        else:
+            return False
+    return True
+
+w1=input()
+w2=input()
+print(valid_anagram_3(w1,w2))
