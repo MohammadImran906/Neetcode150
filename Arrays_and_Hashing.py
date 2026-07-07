b4e2d289930d04080190ab1ef09ee4f60511807a
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-07 17:34:43 +0530

    Top K Frequent Elements two ways

--- Arrays_and_Hashing.py
+++ Arrays_and_Hashing.py
@@ -128,3 +128,24 @@
 # arr=[int(x) for x in input().split()]
 # k=int(input())
 # print(top_k_most_frequent(arr,k))
+
+
+# optimal solution. TC->O(n)  SC->O(n)
+from collections import Counter
+def top_k_most_frequent(arr,k):
+    count=Counter(arr)
+    buckets=[[] for _ in range(len(arr) + 1 )]
+    
+    for num,freq in count.items():
+        buckets[freq].append(num)
+    
+    result=[]
+    for i in range(len(buckets)-1,0,-1):
+        for num in buckets[i]:
+            result.append(num)
+            if len(result)==k:
+                return result
+
+arr=[int(x) for x in input().split()]
+k=int(input())
+print(top_k_most_frequent(arr,k))
