234d4ba02e7539c60c20431ef60410f6bd460024
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-09 21:44:17 +0530

    Valid Sudoku

--- Arrays_and_Hashing.py
+++ Arrays_and_Hashing.py
@@ -218,32 +218,36 @@
 
 
 # Q-8. Valid Sudoku
-def isValidSudoku(board):
-    rows= [0]* 9
-    column= [0]* 9
-    squares= [0]* 9
-    for r in range(9):
-        for c in range(9):
-            if board[r][c]==".":
-                continue
-            val=int(board[r][c])-1
+#For N*N sudoku- TC-> O(n²) SC->O(n)
+#For 9*9 sudoku- TC-> O(1) SC->O(1)
+# def isValidSudoku(board):
+#     rows= [0]* 9
+#     column= [0]* 9
+#     squares= [0]* 9
+#     for r in range(9):
+#         for c in range(9):
+#             if board[r][c]==".":
+#                 continue
+#             val=int(board[r][c])-1
     
-            if (1<<val) & rows[r]:
-                return False
-            if (1<<val) & column[c]:
-                return False
-            if (1<<val) & squares[(r//3)*3 + (c//3)]:
-                return False
+#             if (1<<val) & rows[r]:
+#                 return False
+#             if (1<<val) & column[c]:
+#                 return False
+#             if (1<<val) & squares[(r//3)*3 + (c//3)]:
+#                 return False
     
-            rows[r] |= (1<<val)
-            column[c] |= (1<<val)
-            squares[(r//3)*3 + (c//3)] |= (1<<val)
+#             rows[r] |= (1<<val)
+#             column[c] |= (1<<val)
+#             squares[(r//3)*3 + (c//3)] |= (1<<val)
 
-    return True
+#     return True
 
-board=[input().split() for i in range(9)]
+# board=[input().split() for i in range(9)]
 
-if isValidSudoku(board):
-    print("Valid")
-else:
-    print("Invalid")
+# if isValidSudoku(board):
+#     print("Valid")
+# else:
+#     print("Invalid")
+
+
