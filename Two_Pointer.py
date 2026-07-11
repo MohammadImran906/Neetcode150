66b72fb1d01e91aec2c62bebcf970e98b188c9c9
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-11 22:48:38 +0530

    Two sum and Three sum

--- Two_Pointer.py
+++ Two_Pointer.py
@@ -1,17 +1,78 @@
 #Q-1. Valid Palindrome
 #TC-> O(n) SC->O(1)
-def palindrome(word):
-    n=len(word)
-    left=0
-    right=n-1
+# def valid_palindrome(word):
+#     n=len(word)
+#     left=0
+#     right=n-1
 
-    while left<right:
-        if word[left] != word[right]:
-            return False
-        left +=1
-        right-=1
+#     while left<right:
+#         if word[left] != word[right]:
+#             return False
+#         left +=1
+#         right-=1
 
-    return True
+#     return True
 
-word=input()
-print(palindrome(word))
+# word=input()
+# if valid_palindrome(word):
+#     print("Valid")
+# else:
+#     print("Invalid")
+
+
+
+#Q-2. Two Sum II Input Array Is Sorted
+# def two_sum(arr,x):
+#     n=len(arr)
+#     start=0
+#     end=n-1
+#     while start<end:
+#         current_sum=arr[start] + arr[end]
+
+#         if current_sum==x:
+#             return start, end
+#         elif current_sum< x:
+#             start+=1
+#         else:
+#             end-=1
+#     return []
+
+# arr=[int(x) for x in input().split()]
+# x=int(input())
+# print(two_sum(arr,x))
+
+
+
+#Q-3. 3 Sum
+# TC-> O(n²), Aux.SC->O(1), Internal SC->O(n)
+def three_sum(arr):
+    arr.sort()
+    n=len(arr)
+    result=[]
+    for i in range(n):
+
+        if i > 0 and arr[i] == arr[i - 1]:
+            continue
+
+        start=i+1
+        end=n-1
+
+        while start<end:
+            current_sum=arr[i] + arr[start]+ arr[end]
+            if current_sum==0:
+                result.append([arr[i],arr[start],arr[end]])
+                start+=1
+                end-=1
+
+                while start<end and arr[start]==arr[start-1]:
+                    start+=1
+                while start<end and arr[end]==arr[end+1]:
+                    end-=1
+            elif current_sum<0:
+                start+=1
+            else:
+                end-=1
+    return result
+
+arr=[int(x) for x in input().split()]
+print(three_sum(arr))
