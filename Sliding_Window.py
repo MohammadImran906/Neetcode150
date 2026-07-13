bff977ab96e4c1ead7110a61579eb8eea1f74637
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-13 15:52:22 +0530

    Best Time to Buy And Sell Stock

--- Sliding_Window.py
+++ Sliding_Window.py
@@ -1,20 +1,21 @@
 #Q-1. Best Time to Buy And Sell Stock
-def Buy_Sell(prices):
-    n=len(prices)
-    left=0
-    right=1
-    max_profit=0
-    while right<n:
-        if prices[left]<prices[right]:
-            current_profit=prices[right]-prices[left]
-            max_profit=max(current_profit,max_profit)
-        else:
-            left=right
+#TC->O(n)   SC-> O(1) 
+# def Buy_Sell(prices):
+#     n=len(prices)
+#     left=0
+#     right=1
+#     max_profit=0
+#     while right<n:
+#         if prices[left]<prices[right]:
+#             current_profit=prices[right]-prices[left]
+#             max_profit=max(current_profit,max_profit)
+#         else:
+#             left=right
         
-        right+=1
+#         right+=1
     
-    return max_profit
+#     return max_profit
 
 
-prices=[int(x) for x in input().split()]
-print(Buy_Sell(prices))
+# prices=[int(x) for x in input().split()]
+# print(Buy_Sell(prices))
