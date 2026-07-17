52dc7ed93653fd5b5e99f2005ab08583afa17ad6
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-17 17:57:30 +0530

    #Q-4. Permutation In String

--- Sliding_Window.py
+++ Sliding_Window.py
@@ -93,29 +93,29 @@
 
 #Q-4. Permutation In String
 # TC-> O(n) SC-> O(1)
-def check_permutation(s1,s2):
-    if len(s1)>len(s2):
-        return False
-    s1_count={}
-    s2_count={}
-    for i in range(len(s1)):
-        s1_count[s1[i]]= 1 + s1_count.get(s1[i],0)
-        s2_count[s2[i]]= 1 + s2_count.get(s2[i],0)
-    if s1_count==s2_count:
-        return True
-    l=0 
-    for r in range(len(s1), len(s2)):
-        s2_count[s2[r]]=1 + s2_count.get(s2[r],0)
-        s2_count[s2[l]] -= 1
+# def check_permutation(s1,s2):
+#     if len(s1)>len(s2):
+#         return False
+#     s1_count={}
+#     s2_count={}
+#     for i in range(len(s1)):
+#         s1_count[s1[i]]= 1 + s1_count.get(s1[i],0)
+#         s2_count[s2[i]]= 1 + s2_count.get(s2[i],0)
+#     if s1_count==s2_count:
+#         return True
+#     l=0 
+#     for r in range(len(s1), len(s2)):
+#         s2_count[s2[r]]=1 + s2_count.get(s2[r],0)
+#         s2_count[s2[l]] -= 1
         
-        if  s2_count[s2[l]]==0:
-            s2_count.pop(s2[l])
-        l+=1
+#         if  s2_count[s2[l]]==0:
+#             s2_count.pop(s2[l])
+#         l+=1
 
-        if s1_count==s2_count:
-            return True
-    return False
+#         if s1_count==s2_count:
+#             return True
+#     return False
 
-string1=input()
-string2=input()
-print(check_permutation(string1,string2))
+# string1=input()
+# string2=input()
+# print(check_permutation(string1,string2))
