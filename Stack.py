caf3539f21e56bc8e6343d758f53b70ef1cde6f7
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-19 15:21:13 +0530

    Valid Parantheses

--- Stack.py
+++ Stack.py
@@ -1,6 +1,26 @@
 #Q-1 Valid Parantheses
+#TC->O(n)  SC-> O(1)
 def Valid_parantheses(string):
-
-    return
+    stack=[]
+    for i in string:
+        if i=='(' or i=='{' or i=='[' or i=='<':
+            stack.append(i)
+        else:
+            if len(stack)==0:
+                return False
+            if i==')' and stack[-1]=='(':
+                stack.pop()
+            elif i=='}' and stack[-1] =='{':
+                stack.pop()
+            elif i==']' and stack[-1] =='[':
+                stack.pop()
+            elif i=='>' and stack[-1] =='<':
+                stack.pop()
+            else:
+                return False
+    if len(stack)==0:
+        return True
+    else:
+        return False
 string=input()
 print(Valid_parantheses(string))
