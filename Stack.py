de4bdb250aefcd989c89c5ddd2503b7d2f8379c9
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-26 17:54:57 +0530

    Min Stack

--- Stack.py
+++ Stack.py
@@ -33,20 +33,27 @@
 
 #Q-2. Min Stack two stack approach
 #TC-> O(1)  SC-> O(n)
-class minstack:
-    def __init__(self):
-        self.main_stack=[]
-        self.min_stack=[]
-    def push(self,val):
-        self.main_stack.append(val)
-        if len(self.min_stack)==0 or val<=self.min_stack:
-            self.min_stack.append(val)
-    def pop(self):
-        if self.main_stack[-1]==self.min_stack[-1]:
-            self.min_stack.pop()
-        self.main_stack.pop()
-    def top(self):
-        return self.main_stack[-1]
-    def getMin(self):
-        return self.min_stack[-1]
+# class Minstack:
+#     def __init__(self):
+#         self.main_stack=[]
+#         self.min_stack=[]
+#     def push(self,val):
+#         self.main_stack.append(val)
+#         if len(self.min_stack)==0 or val <= self.min_stack[-1]:
+#             self.min_stack.append(val)
+#     def pop(self):
+#         if self.main_stack[-1]==self.min_stack[-1]:
+#             self.min_stack.pop()
+#         self.main_stack.pop()
+#     def top(self):
+#         return self.main_stack[-1]
+#     def getMin(self):
+#         return self.min_stack[-1]
 
+# obj=Minstack()
+# obj.push(4)
+# obj.push(9)
+# obj.push(1)
+# print(f" Min is {obj.getMin()} ")
+# obj.pop()
+# print(f" Top of stack is {obj.top()} ")
