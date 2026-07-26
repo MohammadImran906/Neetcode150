f6ec0ce49dfd06e4e419bab117021f4961ac58ce
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-26 17:35:31 +0530

    Min Stack

--- Stack.py
+++ Stack.py
@@ -28,3 +28,25 @@
 
 # string=input()
 # print(Valid_parantheses(string))
+
+
+
+#Q-2. Min Stack two stack approach
+#TC-> O(1)  SC-> O(n)
+class minstack:
+    def __init__(self):
+        self.main_stack=[]
+        self.min_stack=[]
+    def push(self,val):
+        self.main_stack.append(val)
+        if len(self.min_stack)==0 or val<=self.min_stack:
+            self.min_stack.append(val)
+    def pop(self):
+        if self.main_stack[-1]==self.min_stack[-1]:
+            self.min_stack.pop()
+        self.main_stack.pop()
+    def top(self):
+        return self.main_stack[-1]
+    def getMin(self):
+        return self.min_stack[-1]
+
