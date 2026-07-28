bc4d13742ad04e696fa27470d2479324c696ea20
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-28 23:08:29 +0530

    Car Fleet

--- Stack.py
+++ Stack.py
@@ -100,8 +100,7 @@
 
 
 
-#Q-5. Car fleet
-#TC-> O(n) SC-> O(n)
+#Q-5. Car fleet. TC-> O(n) SC-> O(n)
 # def car_fleet(target,pos,spd):
 #     cars=sorted(zip(pos,spd), reverse=True)
 #     stack=[]
