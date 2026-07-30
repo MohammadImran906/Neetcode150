602ef60f16143d8a2eba1bf4210593dbdb906d4e
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-30 18:19:04 +0530

    Search in 2-D Matrix optimal approach

--- Binary_search.py
+++ Binary_search.py
@@ -21,19 +21,42 @@
 
 #Q-2. Search a 2-D matrix
 #Better approach(Row-by-Row Binary search). TC-> O(M log N) SC->O(1)
-def Search_2D(matrix,target):
-    for i in matrix:
-        l=0
-        r=len(matrix[0])-1
-        if i[0] <= target <=i[-1]:
-            while l<=r:
-                mid=(l+r)//2
-                if i[mid]==target:
-                    return True
-                elif i[mid]<target:
-                    l=mid+1
-                else:
-                    r=mid-1
+# def Search_2D_1(matrix,target):
+#     for i in matrix:
+#         l=0
+#         r=len(matrix[0])-1
+#         if i[0] <= target <=i[-1]:
+#             while l<=r:
+#                 mid=(l+r)//2
+#                 if i[mid]==target:
+#                     return True
+#                 elif i[mid]<target:
+#                     l=mid+1
+#                 else:
+#                     r=mid-1
+#     return False
+
+
+# Optimal Approach(Binary search) 
+#Tc->O(log(m*n))  SC-> O(1)
+def Search_2D_2(matrix,target):
+    rows=len(matrix)
+    cols=len(matrix[0])
+
+    l=0
+    r=(rows*cols)-1
+    while l<=r:
+        mid=(l+r)//2
+
+        row=mid//cols
+        col=mid%cols
+
+        if matrix[row][col]==target:
+            return True
+        elif matrix[row][col]<target:
+            l=mid+1
+        else:
+            r=mid-1
     return False
 
 n=int(input())
@@ -42,4 +65,4 @@ for i in range(n):
     rows=[int(x) for x in input().split()]
     matrix.append(rows)
 target=int(input())
-print(Search_2D(matrix,target))
+print(Search_2D_2(matrix,target))
