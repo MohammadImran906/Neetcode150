5fda27e5cb7cfdb271e39f6a37b453aae58be754
Mohammad Imran <mohdimran00m@gmail.com>
2026-07-30 16:28:02 +0530

    Binary Search

--- Binary_search.py
+++ Binary_search.py
@@ -1,18 +1,18 @@
 #Q-1. Binary search
 #TC-> O(logn) SC-> O(1)
-def binary_search(arr,k):
-    l=0
-    r=len(arr)-1
-    while l<=r:
-        mid=(l+r)//2
-        if arr[mid]==k:
-            return mid
-        elif arr[mid]<k:
-            l=mid+1
-        else:
-            r=mid-1
-    return False
+# def binary_search(arr,k):
+#     l=0
+#     r=len(arr)-1
+#     while l<=r:
+#         mid=(l+r)//2
+#         if arr[mid]==k:
+#             return mid
+#         elif arr[mid]<k:
+#             l=mid+1
+#         else:
+#             r=mid-1
+#     return False
 
-arr=[int(x) for x in input().split()]
-k=int(input())
-print(binary_search(arr,k))
+# arr=[int(x) for x in input().split()]
+# k=int(input())
+# print(binary_search(arr,k))
