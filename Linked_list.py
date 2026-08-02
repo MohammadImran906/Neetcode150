604a1ea1d9b55edc92a46a5076265de4adff5dcc
Mohammad Imran <mohdimran00m@gmail.com>
2026-08-02 14:59:52 +0530

    Reverse Linked List

--- Linked_list.py
+++ Linked_list.py
@@ -26,3 +26,40 @@
 # Node2.next=Node3
 
 # ll.display_list()
+
+
+
+#Q-1. Reverse Linked List
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+def reverse_list(head):
+    prev=None
+    curr=head
+    while curr:
+        nxt=curr.next
+        curr.next=prev
+        prev=curr
+        curr=nxt
+    return prev
+
+def print_list(head):
+    temp=head
+    while temp:
+        print(temp.data,end="->")
+        temp=temp.next
+    print("None")
+
+Node1=Node(10)
+Node2=Node(20)
+Node3=Node(30)
+
+head=Node1
+head.next=Node2
+Node2.next=Node3
+
+print_list(head)
+reverse=reverse_list(head)
+print_list(reverse)
+
