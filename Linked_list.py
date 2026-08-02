7e1d791deb0005997e46149fda0120f30e7f9e53
Mohammad Imran <mohdimran00m@gmail.com>
2026-08-02 14:10:24 +0530

    Creating and Printing Linked List

--- Linked_list.py
+++ Linked_list.py
@@ -1,28 +1,28 @@
 #Creating and Printing Linked list
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
+# class Node:
+#     def __init__(self,data):
+#         self.data=data
+#         self.next=None
 
-class LinkedList:
-    def __init__(self):
-        self.head=None
+# class LinkedList:
+#     def __init__(self):
+#         self.head=None
 
-    def display_list(self):
-        curr=self.head
-        while curr:
-            print(curr.data, end="->")
-            curr=curr.next
-        print("None")
+#     def display_list(self):
+#         curr=self.head
+#         while curr:
+#             print(curr.data, end="->")
+#             curr=curr.next
+#         print("None")
 
-ll=LinkedList()
+# ll=LinkedList()
 
-Node1=Node(10)
-Node2=Node(20)
-Node3=Node(30)
+# Node1=Node(10)
+# Node2=Node(20)
+# Node3=Node(30)
 
-ll.head=Node1
-ll.head.next=Node2
-Node2.next=Node3
+# ll.head=Node1
+# ll.head.next=Node2
+# Node2.next=Node3
 
-ll.display_list()
+# ll.display_list()
