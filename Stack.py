ab1836eab1354f2ea865232e4409a24d5b5b1fc0
Mohammad Imran <mohdimran00m@gmail.com>
2026-09-14 18:54:08 +0530

    Largest rectangle in histogram

--- Stack.py
+++ Stack.py
@@ -118,8 +118,7 @@
 # print(car_fleet(target,position,speed))
 
 
-#Q-6. Largest rectangle in histogram
-#TC->O(n) SC->O(n)
+#Q-6. Largest rectangle in histogram TC->O(n) SC->O(n)
 # def lar_rect_in_hist(heights):
 #     max_area=0
 #     stack=[]
